d7ffb08cb8c550f269226b4437ce3e533e611f85
Samruddhi Malvankar <165154110+samruddhi33333@users.noreply.github.com>
2025-02-13 09:50:02 +0530

    Delete src directory

--- src/ApolloClient.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql", // Change to backend URL if deployed
-  cache: new InMemoryCache(),
-});
-
-export default client;
